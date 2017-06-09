8868595165539f80911bc8a3622486da189b5fe0
rrarangel <rrarangel@gmail.com>
2017-06-09 17:39:26 -0300

    arquivos iniciais

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-<script>
-    $(function(){
+
+$(function(){
         //alert("Bem Vindo!");
-    });
-</script>
+    
+    //remove o fundo escurecido
+    $("#mod01").modal({backdrop:false, keyboard:false});
+    
+    //Abre a janela modal
+    $("#mod01").modal("show");
+    
+    //fecha a janela modal apos 3 segundos
+    setTimeout(function(){ 
+               $("#mod01").modal("hide")},3000);
+    
+});
