f6c786af8bd6578d205a99f42f9589ec4952c836
rrarangel <rrarangel@gmail.com>
2017-06-14 17:38:11 -0300

    arquivos iniciais

--- ProjetoPratico/js/main.js
+++ ProjetoPratico/js/main.js
@@ -1,38 +1,7 @@
-
-$(function(){
-        //alert("Bem Vindo!");
-    
-    //remove o fundo escurecido
-    $("#mod01").modal({backdrop:false, keyboard:false});
-    
-    //Abre a janela modal
-    $("#mod01").modal("show");
-    
-    //fecha a janela modal apos 3 segundos
-    setTimeout(function(){ 
-               $("#mod01").modal("hide")},3000);
-    
-    $(".tooltip1").tooltip({
-        animation:false,
-        //delay : {show: 100, hide:2000},
-        title: "Titulo padrão",
-        //trigger : "over"
-    });
-    
-    $(".ppv").popover({
-        title: "Titulo do PopOver",
-        trigger: 'hover focus'
-    });
-    
-    $(".btnjs").button();
-    
-    $("#troca-estado").click(function(){
-        var btn = $(this);
-        btn.button("loading");
-        
-        setTimeout(function(){
-                   btn.button("reset");
-                   },3000);
-    });
-    
+var $doc = $('html, body');
+$('.scrollSuave').click(function() {
+    $doc.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 1000);
+    return false;
 });
